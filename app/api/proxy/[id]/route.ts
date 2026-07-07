c6386cbba5a60c122a395bc5edca93fe6dbaafbf
hasiniainafenovolantsoa-debug <hasiniainafenovolantsoa@gmail.com>
2026-07-07 09:48:35 +0300

    Change request type to any in GET function
    Changed request type from NextRequest to any for flexibility.

--- app/api/proxy/[id]/route.ts
+++ app/api/proxy/[id]/route.ts
@@ -1,12 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { supabase } from '../../../../lib/supabase';
 
 export async function GET(
-  request: NextRequest, 
+  request: any, // <--- Ovay ho 'any'
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
-
   const { data, error } = await supabase
     .from('movies') 
     .select('*')
